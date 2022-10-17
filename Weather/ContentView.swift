4f039cc1af333bb00e69ae0f8c17c302af0a5f7c
taylorJ <taylor.ji719@gmail.com>
2022-10-16 23:58:00 -0700

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Taylor Ji on 2022-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
